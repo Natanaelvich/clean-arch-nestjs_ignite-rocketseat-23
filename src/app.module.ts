fdb8e2b1b586f4368187ca44a4fa25944d193a8d
Natanaelvich <taelima1997@gmail.com>
2023-08-27 09:19:23 -0300

    add config env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common'
 import { PrismaService } from './prisma/prisma.service'
+import { ConfigModule } from '@nestjs/config'
+import { envSchema } from './env'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+  ],
   controllers: [],
   providers: [PrismaService],
 })
