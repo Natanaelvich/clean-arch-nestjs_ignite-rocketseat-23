ca7a3126fe1adaf1a3a6b2a27198f9a9302eb2da
natanaelvich <taelima1997@gmail.com>
2023-09-02 21:48:46 -0300

    add domain structure

---
+++ src/domain/forum/application/repositories/questions-repository.ts
@@ -0,0 +1,11 @@
+import { PaginationParams } from '@/core/repositories/pagination-params'
+import { Question } from '@/domain/forum/enterprise/entities/question'
+
+export abstract class QuestionsRepository {
+  abstract findById(id: string): Promise<Question | null>
+  abstract findBySlug(slug: string): Promise<Question | null>
+  abstract findManyRecent(params: PaginationParams): Promise<Question[]>
+  abstract save(question: Question): Promise<void>
+  abstract create(question: Question): Promise<void>
+  abstract delete(question: Question): Promise<void>
+}
