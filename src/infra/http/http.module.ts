7a225f7a4bf1953c8740f2dd883b1e4c5a5e256d
natanaelvich <taelima1997@gmail.com>
2023-09-18 21:24:07 -0300

    add more providers and controllers

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -14,6 +14,14 @@ import { EditQuestionUseCase } from '@/domain/forum/application/use-cases/edit-q
 import { GetQuestionBySlugUseCase } from '@/domain/forum/application/use-cases/get-question-by-slug'
 import { EditQuestionController } from './controllers/edit-question.controller'
 import { GetQuestionBySlugController } from './controllers/get-question-by-slug.controller'
+import { AnswerQuestionUseCase } from '@/domain/forum/application/use-cases/answer-question'
+import { DeleteAnswerUseCase } from '@/domain/forum/application/use-cases/delete-answer'
+import { DeleteQuestionUseCase } from '@/domain/forum/application/use-cases/delete-question'
+import { EditAnswerUseCase } from '@/domain/forum/application/use-cases/edit-answer'
+import { AnswerQuestionController } from './controllers/answer-question.controller'
+import { DeleteAnswerController } from './controllers/delete-answer.controller'
+import { DeleteQuestionController } from './controllers/delete-question.controller'
+import { EditAnswerController } from './controllers/edit-answer.controller'
 
 @Module({
   imports: [DatabaseModule, CryptographyModule],
@@ -24,6 +32,10 @@ import { GetQuestionBySlugController } from './controllers/get-question-by-slug.
     FetchRecentQuestionsController,
     GetQuestionBySlugController,
     EditQuestionController,
+    DeleteQuestionController,
+    AnswerQuestionController,
+    EditAnswerController,
+    DeleteAnswerController,
   ],
   providers: [
     CreateQuestionUseCase,
@@ -32,6 +44,10 @@ import { GetQuestionBySlugController } from './controllers/get-question-by-slug.
     AuthenticateStudentUseCase,
     GetQuestionBySlugUseCase,
     EditQuestionUseCase,
+    DeleteQuestionUseCase,
+    AnswerQuestionUseCase,
+    EditAnswerUseCase,
+    DeleteAnswerUseCase,
   ],
 })
 export class HttpModule {}
