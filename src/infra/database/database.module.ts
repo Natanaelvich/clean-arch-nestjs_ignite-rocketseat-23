9bb872ec8392efc6041166afb1b4ab63271866b3
natanaelvich <taelima1997@gmail.com>
2023-09-23 21:40:39 -0300

    update providers with AttachmentsRepository

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -14,6 +14,7 @@ import { AnswerCommentsRepository } from '@/domain/forum/application/repositorie
 import { AnswersRepository } from '@/domain/forum/application/repositories/answers-repository'
 import { QuestionAttachmentsRepository } from '@/domain/forum/application/repositories/question-attachments-repository'
 import { QuestionCommentsRepository } from '@/domain/forum/application/repositories/question-comments-repository'
+import { AttachmentsRepository } from '@/domain/forum/application/repositories/attachments-repository'
 
 @Module({
   providers: [
@@ -45,6 +46,10 @@ import { QuestionCommentsRepository } from '@/domain/forum/application/repositor
     {
       provide: AnswerAttachmentsRepository,
       useClass: PrismaAnswerAttachmentsRepository,
+    },
+    {
+      provide: AttachmentsRepository,
+      useClass: PrismaAttachmentsRepository,
     },
   ],
   exports: [
