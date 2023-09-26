2e223e05d46ae10fe32f7c06bf0f96e9d2940657
natanaelvich <taelima1997@gmail.com>
2023-09-25 21:59:26 -0300

    update QuestionCommentsRepository

--- src/domain/forum/application/repositories/question-comments-repository.ts
+++ src/domain/forum/application/repositories/question-comments-repository.ts
@@ -1,5 +1,6 @@
 import { PaginationParams } from '@/core/repositories/pagination-params'
 import { QuestionComment } from '../../enterprise/entities/question-comment'
+import { CommentWithAuthor } from '../../enterprise/entities/value-objects/comment-with-author'
 
 export abstract class QuestionCommentsRepository {
   abstract findById(id: string): Promise<QuestionComment | null>
@@ -7,6 +8,11 @@ export abstract class QuestionCommentsRepository {
     questionId: string,
     params: PaginationParams,
   ): Promise<QuestionComment[]>
+
+  abstract findManyByQuestionIdWithAuthor(
+    questionId: string,
+    params: PaginationParams,
+  ): Promise<CommentWithAuthor[]>
 
   abstract create(questionComment: QuestionComment): Promise<void>
   abstract delete(questionComment: QuestionComment): Promise<void>
