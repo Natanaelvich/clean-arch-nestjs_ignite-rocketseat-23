f0a4cffe0615937882b2bad2f22b4b7e7a38337a
natanaelvich <taelima1997@gmail.com>
2023-09-27 21:53:51 -0300

    add findDetailsBySlug to QuestionsRepository

--- src/domain/forum/application/repositories/questions-repository.ts
+++ src/domain/forum/application/repositories/questions-repository.ts
@@ -1,10 +1,12 @@
 import { PaginationParams } from '@/core/repositories/pagination-params'
 import { Question } from '@/domain/forum/enterprise/entities/question'
+import { QuestionDetails } from '../../enterprise/entities/value-objects/question-details'
 
 export abstract class QuestionsRepository {
   abstract findById(id: string): Promise<Question | null>
   abstract findBySlug(slug: string): Promise<Question | null>
   abstract findManyRecent(params: PaginationParams): Promise<Question[]>
+  abstract findDetailsBySlug(slug: string): Promise<QuestionDetails | null>
   abstract save(question: Question): Promise<void>
   abstract create(question: Question): Promise<void>
   abstract delete(question: Question): Promise<void>
